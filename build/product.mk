5f95866b49720aab8e6301fa5af364e0751d47d6
Alexander Koskovich <zvnexus@outlook.com>
2020-11-09 10:49:50 -0700

    proton: Import QCOM board utilities
    Change-Id: I77eee7bc8892d836e14564d99b13250b426ead35
    Signed-off-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>

--- build/product.mk
+++ build/product.mk
@@ -20,6 +20,7 @@ $(call inherit-product, vendor/proton/overlay/overlay.mk)
 $(call inherit-product, vendor/proton/telephony/telephony.mk)
 $(call inherit-product, vendor/proton/audio/audio.mk)
 $(call inherit-product-if-exists, vendor/proton/signing/dev.mk)
+$(call inherit-product, vendor/proton/build/qcom_boards.mk)
 
 # Flatten APEXs for performance
 OVERRIDE_TARGET_FLATTEN_APEX := true
